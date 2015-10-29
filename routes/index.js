f2fdb11d79350d13f0fa9a7fd0b5326b80e23057
essmahr <essmahr@gmail.com>
2015-10-28 22:15:17 -0700

    disable registration

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,6 @@ router.get('/', function (req, res) {
   res.render('index', { user : req.user });
 });
 
-router.get('/register', function(req, res) {
-  res.render('register', {});
-});
-
-router.post('/register', function(req, res) {
-  User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
-    if (err) {
-      return res.render('register', { user : user });
-    }
-
-    passport.authenticate('local')(req, res, function () {
-      res.redirect('/');
-    });
-  });
-});
-
 router.get('/login', function(req, res) {
   res.render('login', { user : req.user });
 });

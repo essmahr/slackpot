3d76a2aa2b17f568a7e49983e7186cc4bd83948e
essmahr <essmahr@gmail.com>
2015-11-03 22:03:03 -0800

    user actually really has a bot now

--- routes/bot.js
+++ routes/bot.js
@@ -1,17 +1,17 @@
 var express = require('express');
+var Account = require('../models/Account');
 var Bot = require('../models/Bot');
 var Channel = require('../models/Channel');
 var router = express.Router();
 var isAuthenticated = require('../isAuthenticated');
 
-// redirect to home upon visiting /bots
 router.get('/', isAuthenticated, function (req, res) {
   res.redirect('/');
 });
 
-// only allow new bot if user hasn't any
 router.get('/new', isAuthenticated, function (req, res) {
   Bot.find({_owner: req.user.id}, function(err, bots) {
+    // only allow new bot if user hasn't any
     if (bots.length) {
       res.redirect('/')
     } else {
@@ -22,7 +22,7 @@ router.get('/new', isAuthenticated, function (req, res) {
 
 // new bot!
 router.post('/new', isAuthenticated, function (req, res, next) {
-  var channelId = new Channel(req.body.accessToken)
+  var channelId = new Channel(req.user.accessToken)
     .getId(req.body.channel);
 
   var bot = new Bot({
@@ -36,17 +36,25 @@ router.post('/new', isAuthenticated, function (req, res, next) {
     }
   });
 
-  bot.save(function(err) {
-    if (err) {
-      req.flash('error', err.message);
-    }
-    res.redirect('/');
+  bot.save(function(err, bot) {
+    if (err) { req.flash('error', err.message); }
+    Account.findById(req.user._id, function(err, account) {
+      if (err) { req.flash('error', err.message); }
+      account.bots.push(bot.id),
+      account.save(function(err) {
+        if (err) { req.flash('error', err.message); }
+        // http://stackoverflow.com/questions/24493243/update-logged-in-user-details-in-session
+        req.login(account, function(err) {
+          if (err) { req.flash('error', err.message); }
+          res.redirect('/');
+        });
+      })
+    });
   });
 });
 
-//
 router.get('/edit/:id', isAuthenticated, function (req, res) {
-  Bot.findOne({id: req.params.id}, function(err, bot) {
+  Bot.findOne({_id: req.params.id}, function(err, bot) {
     if (err)
       res.send(err);
     if (bot) {
